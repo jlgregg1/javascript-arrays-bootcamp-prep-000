ac79395dcd3bb9a82f03be5dfecd3bd816791baf
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-14 03:00:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,15 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-}
 function addElementToBeginningOfArray(array, element) {
   [element,...array]
+}
+function addElementToEndOfArray(array, element) {
+  [...array,element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
 }
-function addElementToEndOfArray(array, element) {
-  [...array,element]
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
 }
 function accessElementInArray(array, index) {
   console.log(array[index])
