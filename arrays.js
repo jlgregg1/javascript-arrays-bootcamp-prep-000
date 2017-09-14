d1bd728fde6fa9e7cb5af1a4eebd8e1f3661a46a
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-14 02:24:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
-  return element...array
+  console.log(element...array)
 }
 addElementToBeginningOfArray(chocolateBars, 'milkyway')
+
